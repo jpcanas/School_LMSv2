b2caf735c0df8dca659fab91125787ccdd5796ee
June Canas <junecanas2@gmail.com>
2023-05-01 16:20:12 +0800

    adding additional functionalities

--- LMS_v2.1/uiFunctions.py
+++ LMS_v2.1/uiFunctions.py
@@ -1,7 +1,12 @@
-from PyQt6.QtWidgets import QWidget, QPushButton, QFrame, QStackedWidget, QVBoxLayout, QSizeGrip, QGraphicsDropShadowEffect
+from PyQt6.QtWidgets import QWidget, QPushButton, QFrame, QStackedWidget, QVBoxLayout, QSizeGrip, \
+    QGraphicsDropShadowEffect
 from PyQt6.QtGui import QColor
 
 from profilePage import Profile
+from homePage import Home
+from gradesJHSPage import GradesJHS
+from gradesSHSPage import GradesSHS
+from attendance_OVPage import AttendanceOV
 
 
 class UIFunctions:
@@ -61,59 +66,88 @@ class UIFunctions:
         self.gradeSHSPage = self.app_pages.findChild(QWidget, "gradeSHSPage")
         self.attendancePage = self.app_pages.findChild(QWidget, "attendPage")
 
-        self.profileFrame = self.main_ui.findChild(QFrame, "profMainFrame")
-        self.profileLayout = self.main_ui.findChild(QVBoxLayout, "profileMainLayout")
-        self.profileMain = Profile()
-
-        self.profileLayout.addWidget(self.profileMain)
-
+        # Default page to show up
+        self.app_pages.setCurrentWidget(self.homePage)
+        self.homeFunction()
         self.profileFunction()
+        # self.jhsGradeFunction()
+        self.shsGradeFunction()
+        self.attendance_ov()
 
     def registerFunction(self):
         # Register page
         pass
 
     def homeFunction(self):
-        # Home page buttons
-        pass
+        # Home Frame from stacked widgets
+        self.homeFrame = self.main_ui.findChild(QFrame, "homeMainFrame")
+        self.homeLayout = self.main_ui.findChild(QVBoxLayout, "homeMainLayout")
+        self.homeMain = Home()
+        self.homeLayout.addWidget(self.homeMain)
+
+        # Home page buttons inside the home frame
+        self.homeMain.homeProfileBtn.clicked.connect(lambda: self.setPage(self.profilePage))
+        # self.homeMain.homeGradesBtn.clicked.connect(lambda: self.setPage(self.gradeJHSPage))
+        self.homeMain.homeGradesBtn.clicked.connect(lambda: self.setPage(self.gradeSHSPage))
+        self.homeMain.homeAttendanceBtn.clicked.connect(lambda: self.setPage(self.attendancePage))
 
     def profileFunction(self):
-        # Profile page buttons
+        # Profile page buttons (Top Bar)
         self.profileHomeBtn = self.profilePage.findChild(QPushButton, "homeBtnProf")
         self.profileGradeBtn = self.profilePage.findChild(QPushButton, "gradesBtnProf")
-
         self.profileHomeBtn.clicked.connect(lambda: self.setPage(self.homePage))
-        self.profileGradeBtn.clicked.connect(lambda: self.setPage(self.gradeJHSPage))
+        # self.profileGradeBtn.clicked.connect(lambda: self.setPage(self.gradeJHSPage))
+        self.profileGradeBtn.clicked.connect(lambda: self.setPage(self.gradeSHSPage))
+
+        # Profile Page Frame from stacked widgets
+        self.profileFrame = self.main_ui.findChild(QFrame, "profMainFrame")
+        self.profileLayout = self.main_ui.findChild(QVBoxLayout, "profileMainLayout")
+        self.profileMain = Profile()
+        self.profileLayout.addWidget(self.profileMain)
 
     def jhsGradeFunction(self):
         # JHS grades page buttons
         self.jhsHomeBtn = self.gradeJHSPage.findChild(QPushButton, "homeBtnJHS")
         self.jhsProfileBtn = self.gradeJHSPage.findChild(QPushButton, "prevBtnJHS")
         self.jhsAttendanceBtn = self.gradeJHSPage.findChild(QPushButton, "attendBtnJHS")
-
         self.jhsHomeBtn.clicked.connect(lambda: self.setPage(self.homePage))
         self.jhsProfileBtn.clicked.connect(lambda: self.setPage(self.profilePage))
         self.jhsAttendanceBtn.clicked.connect(lambda: self.setPage(self.attendancePage))
 
+        # JHS Grades Page Frame from stacked widgets
+        self.gradesJHSFrame = self.main_ui.findChild(QFrame, "gJHSMainFrame")
+        self.gradesJHSLayout = self.main_ui.findChild(QVBoxLayout, "gradesJHSLayout")
+        self.gradeJHSMain = GradesJHS()
+        self.gradesJHSLayout.addWidget(self.gradeJHSMain)
+
     def shsGradeFunction(self):
         # SHS grades page buttons
-        self.shsHomeBtn = self.gradeJHSPage.findChild(QPushButton, "homeBtnSHS")
-        self.shsProfileBtn = self.gradeJHSPage.findChild(QPushButton, "prevBtnSHS")
-        self.shsAttendanceBtn = self.gradeJHSPage.findChild(QPushButton, "attendBtnSHS")
-
+        self.shsHomeBtn = self.gradeSHSPage.findChild(QPushButton, "homeBtnSHS")
+        self.shsProfileBtn = self.gradeSHSPage.findChild(QPushButton, "prevBtnSHS")
+        self.shsAttendanceBtn = self.gradeSHSPage.findChild(QPushButton, "attendBtnSHS")
         self.shsHomeBtn.clicked.connect(lambda: self.setPage(self.homePage))
         self.shsProfileBtn.clicked.connect(lambda: self.setPage(self.profilePage))
         self.shsAttendanceBtn.clicked.connect(lambda: self.setPage(self.attendancePage))
 
+        # SHS Grades Page Frame from stacked widgets
+        self.gradesSHSFrame = self.main_ui.findChild(QFrame, "gSHSMainFrame")
+        self.gradesSHSLayout = self.main_ui.findChild(QVBoxLayout, "gradesSHSLayout")
+        self.gradeSHSMain = GradesSHS()
+        self.gradesSHSLayout.addWidget(self.gradeSHSMain)
+
     def attendance_ov(self):
         # Attendance and Observed Values page buttons
-        self.attendHomeBtn = self.gradeJHSPage.findChild(QPushButton, "homeBtnAttend")
-        self.attendGradeBtn = self.gradeJHSPage.findChild(QPushButton, "prevBtnAttend")
-
+        self.attendHomeBtn = self.attendancePage.findChild(QPushButton, "homeBtnAttend")
+        self.attendGradeBtn = self.attendancePage.findChild(QPushButton, "prevBtnAttend")
         self.attendHomeBtn.clicked.connect(lambda: self.setPage(self.homePage))
-        self.attendGradeBtn.clicked.connect(lambda: self.setPage(self.gradeJHSPage))
+        # self.attendGradeBtn.clicked.connect(lambda: self.setPage(self.gradeJHSPage))
+        self.attendGradeBtn.clicked.connect(lambda: self.setPage(self.gradeSHSPage))
+
+        # Attendance and Observed Values Page Frame from stacked widgets
+        self.attendFrame = self.main_ui.findChild(QFrame, "attendMainFrame")
+        self.attendFrameLayout = self.main_ui.findChild(QVBoxLayout, "attendMainLayout")
+        self.attendOV_Main = AttendanceOV()
+        self.attendFrameLayout.addWidget(self.attendOV_Main)
 
     def setPage(self, page):
         self.app_pages.setCurrentWidget(page)
-
-
